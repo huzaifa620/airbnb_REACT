32b18d9faf8d6b0dbd9e0b49700ad463f6b45740
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-03 03:15:32 +0500

    header completed

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -5,8 +5,8 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <Home />
       <Header />
+      <Home />
     </div>
   );
 }
--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Avatar } from '@mui/material';
 
 const Header = () => {
   return (
     <div className='header'>
         <img className='header_icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/220px-Airbnb_Logo_B%C3%A9lo.svg.png' alt='logo'/>
+
+        <div className='header_center'>
+            <input type='text' />
+            <SearchIcon />
+        </div>
+
+        <div className='header_right'>
+            <LanguageIcon />
+            <p>Become a Host</p>
+            <ExpandMoreIcon />
+            <Avatar />
+        </div>
     </div>
   )
 }
