71854bd05316581fa28d8617ea106f911bfbc4e4
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-04 01:36:53 +0500

    date picker added

--- airbnb-clone/src/Search.js
+++ airbnb-clone/src/Search.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
+import { DateRangePicker } from 'react-date-range'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
 const Search = () => {
-  return (
-    <div className='search'>
-        
-    </div>
-  )
+
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection"
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    return (
+        <div className='search'>
+            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect}/>
+
+        </div>
+    )
 }
 
 export default Search
