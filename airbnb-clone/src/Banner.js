413eda533ccf37fae48b6c23e296cf28fd33c190
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-04 01:21:32 +0500

    home paused

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import { Button } from '@mui/material';
+import Search from './Search'; 
 
 const Banner = () => {
+
+  const [showSearch, setShowSearch] = useState(false)
+
   return (
     <div className='banner'>
 
         <div className='banner_search'>
-            <Button className='banner_searchButton' variant='outlined'>Search Dates</Button>
+            {showSearch && <Search />}
+            <Button className='banner_searchButton' variant='outlined' onClick={() => setShowSearch(!showSearch)}>Search Dates</Button>
         </div>
 
         <div className='banner_info'>
