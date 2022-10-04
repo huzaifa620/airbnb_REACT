a9fc2502a9e819671cb3d00205368df7f30b2d5f
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-04 18:26:17 +0500

    link added

--- airbnb-clone/src/Header.js
+++ airbnb-clone/src/Header.js
@@ -4,11 +4,15 @@ import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Avatar } from '@mui/material';
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div className='header'>
-        <img className='header_icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/220px-Airbnb_Logo_B%C3%A9lo.svg.png' alt='logo'/>
+    <div className='header'>  
+
+        <Link to='/'>
+          <img className='header_icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/220px-Airbnb_Logo_B%C3%A9lo.svg.png' alt='logo'/>
+        </Link>
 
         <div className='header_center'>
             <input type='text' />
