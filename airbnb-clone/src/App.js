ccb6dc0cd28883f14b370fd114ed202832e5bb2f
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-04 05:55:03 +0500

    search page initialized

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ airbnb-clone/src/SearchPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './SearchPage.css'
+import Button from "@mui/material/Button"
+
+const SearchPage = () => {
+  return (
+    <div className='searchPage'>
+      <div className='searchPage_info'>
+        <p> 62 stays . 26 august to 30 august </p>
+        <h1> Stays nearby </h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchPage
